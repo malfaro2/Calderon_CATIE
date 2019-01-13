fa8013d3ec42f2828ad0302ac57c6b54b2ae5fd9
ecamo19 <erickcamo19@gmail.com>
2019-01-12 23:48:19 -0600

    CWM

--- scripts/analysis_fric_feve_fdis_127plots.R
+++ scripts/analysis_fric_feve_fdis_127plots.R
@@ -71,26 +71,31 @@ rowSums(dabund_relativa[12,dabund_relativa[12,]>0])
 
 
 #Se elige la CWM del trait 2, parcela 1
-weighted.mean(deff_clean[colnames(dabund_relativa),2],dabund_relativa[1,])
+weighted.mean(deff_clean[colnames(dabund_relativa),127],dabund_relativa[1,])#wRONG
+weighted.mean(deff_clean[colnames(dabund_relativa),1],dabund_relativa[127,])#wRONG
+
 
 # CWM del las 127 parcelas weight por abund relativa ----------------------
 # Funcion CWM -------------------------------------------------------------
 
-cwm.func<- function(x){
+#af
+cwm.func1<- function(x){
   weighted.mean(deff_clean[names(x[x>0]),1],x[x>0],na.rm = T)
 }
 
+#afe
+cwm.func2<- function(x){
+  weighted.mean(deff_clean[names(x[x>0]),2],x[x>0],na.rm = T)
+}
+
+#cfms
+cwm.func3<- function(x){
+  weighted.mean(deff_clean[names(x[x>0]),3],x[x>0],na.rm = T)
+}
+
+
 apply(dabund_relativa, 1, cwm.func) 
 as.data.frame(apply(dabund, 1, cwm.func)) 
-
-
-
-
-
-
-
-
-
 
 
 
