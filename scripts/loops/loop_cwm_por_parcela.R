00c95a1d72038c5b1604826f846d86cd45a91c33
ronny <ronny@ixpantia.com>
2019-01-22 08:31:29 -0600

    nuevo loop para obtener lista con parcelas y especies >0

--- scripts/loops/loop_cwm_por_parcela.R
+++ scripts/loops/loop_cwm_por_parcela.R
@@ -16,46 +16,66 @@ xy_plot <- xy_plot %>%
 dabund_relativa[1,]
 
 #Preliminar 
-names(dabund_relativa[1,dabund_relativa[1,]>0])
-(plot1 <- deff_clean[names(dabund_relativa[1,dabund_relativa[1,]>0]),])
+names(dabund_relativa[1, dabund_relativa[1, ] > 0])
+(plot1 <- deff_clean[names(dabund_relativa[1,dabund_relativa[1,] > 0]),])
 
 
 #Poner en una lista 
 
 #Crear una lista por cada parcela
-list<-split(dabund_relativa, row.names(dabund_relativa))
+list <- split(dabund_relativa, row.names(dabund_relativa))
 length(list)
 #str(list)
 
 #Crear una lista vacia
-sp_cwm_eff<-list()
+sp_cwm_eff <- list()
 
-#Loop 
-for (i in seq_along(names(list))) {
-  
+traits <- list()
+completa <- list()
+#Loop
+
+# Loop para traer lista con parcelas y su seleccion de especies con
+# abundancia mayor a cero
+for (i in seq_along(list)) {
   #Rasgos funcioanles de las especies presentes de cada parcela con
   #una abundancia mayor a 0 
-  traits<- deff_clean[names(list[[i]][,list[[i]]>0]),]
+  nombres_parcelas <- names(list[i])
+  traits[[nombres_parcelas]] <- deff_clean[names(list[[i]][ , list[[i]] > 0]),]
   
-  # calcular CWM por abundancia relativa en cada parcela 
-  cwm <- apply(traits, 2, weighted.mean)
-  
-  #Nombres de cada parcela
-  plot<- unique(row.names(list[[i]][,list[[i]]>0]))
-  
-  #Nombres de los traits
-  traits<- unique(colnames(deff_clean))
-  
-  #datos con parcela y composicion de especies 
-  data<-data.frame(cwm,plot,traits)
-  
-  #Resultado
-  sp_cwm_eff[[i]] <- data
-  #print(sp_comp)
 }
 
+# Loop para aplicar funcion de weighted mean en cada uno de las parcelas
+for (parcela in seq_along(traits)) {
+  a <- weighted.mean(traits[[parcela]], dabund_clean)
+  print(a)
+}
+  
+  
+   # for (parcela in )
+   # weighted.mean(traits, dabund_clean)
+
+   # calcular CWM por abundancia relativa en cada parcela
+   # cwm <- apply(traits, 2, (weighted.mean))
+   # 
+   # #Nombres de cada parcela
+   # plot <- unique(row.names(list[[i]][,list[[i]]>0]))
+   # 
+   # #Nombres de los traits
+   # traits <- unique(colnames(deff_clean))
+  
+   #datos con parcela y composicion de especies
+   # data <- data.frame(traits)
+  
+   #Resultado
+   # sp_cwm_eff[[i]] <- data
+   #print(sp_comp)
+}
+sp_cwm_eff
+sp_cwm_eff[[1]]
+
+
 #convertir lista a data.frame
-data_cwm<- plyr::ldply (sp_cwm_eff, data.frame)
+data_cwm <- plyr::ldply(sp_cwm_eff, data.frame)
 head(data_cwm)
 head(xy_plot)
 
