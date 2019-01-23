a7586d1fd771b0f17622b1e29fc0b45abe228906
ecamo19 <erickcamo19@gmail.com>
2019-01-23 12:19:04 -0600

    Los loops realizados para CWM y FDiver esta incorrectos por lo que se van a repetir los analisis con el paquete FD

---
+++ analisis_FDiversidad.R
@@ -0,0 +1,8 @@
+rm(list = ls())
+
+# Objetivo ----------------------------------------------------------------
+#Calcular los indices de diversidad funcional FDis, FDiv, FEve y CWM para 
+#todas las 127 parcelas del norte de Costa Rica mediante el paquete FD
+
+
+#Paquetes
