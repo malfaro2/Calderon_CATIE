869052eddcf669341a608b8449a2a69ec24617a0
ecamo19 <erickcamo19@gmail.com>
2019-01-30 20:14:43 -0600

    Functional redundancy

---
+++ scripts/analisis_redundancia_funcional_sinpalmas.R
@@ -0,0 +1,55 @@
+rm(list = ls())
+
+
+# Argumentos de la funcion ------------------------------------------------
+
+#comm	A matrix or a data frame of N plots × S species containing the 
+#abundance or incidence (0/1) of all species in the in plots. 
+#Columns are species and plots are rows
+
+#dis	An object of class 'dist' containing the functional distances 
+#among species
+
+#tol	A tolerance threshold (a value less than tol is considered as null)
+
+#abundance	A logical. If TRUE abundance data are used when available; 
+#if FALSE incidence (0/1) data are used.
+
+#   -----------------------------------------------------------------------
+
+
+# Objetivo ----------------------------------------------------------------
+#El objetivo de este script es calcular el uniqueness y redundancy para los 
+#datos sin palmas de las 127 parcelas
+
+
+# cargar funcion uniqueness -----------------------------------------------
+source("scripts/functions/function_functional_redundancy_original.R")
+
+# Cargar data -------------------------------------------------------------
+
+source("scripts/data_cleaning_for_loops.R")
+xy_plot <- read.csv("data/raw/data_posicion_parcelas.csv")
+
+
+# comm --------------------------------------------------------------------
+#Data parcelas por especies
+#Columns are species and plots are rows 
+
+abundancia <- read.table("clipboard", header=T)
+abundancia <-   t(abundancia)
+
+
+# dis ---------------------------------------------------------------------
+#Data rasgos: se deben convertir objeto dis
+#dis: An object of class 'dist' containing the functional distances among 
+#species
+traits <- read.table("clipboard", header=T)
+traits
+
+#Transformar traits a distancias
+dist <- dist(, method = "euclidean") 
+
+
+# Funcion -----------------------------------------------------------------
+Uni <- uniqueness(com, dis, abundance=TRUE)
