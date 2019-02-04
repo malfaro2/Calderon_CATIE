41158fe23e3e1b7083aa40b766217b465a3ed032
ecamo19 <erickcamo19@gmail.com>
2019-02-03 20:02:12 -0600

    Ref #5

--- scripts/statiscal_analysis/moran_test_spatial_dependency_cwm.R
+++ scripts/statiscal_analysis/moran_test_spatial_dependency_cwm.R
@@ -12,8 +12,8 @@ library(ape)
 
 
 # Cargar datos ------------------------------------------------------------
-ozone <- read.table("https://stats.idre.ucla.edu/stat/r/faq/ozone.csv", sep=",", header=T)
-head(ozone, n=10)
+data_cwm <- read.csv("data/resultados_csv/data_cwm_coord.csv", header=T)
+head(data_cwm, n=10)
 
 
 # Matrix of inverse distance weights --------------------------------------
@@ -21,23 +21,34 @@ head(ozone, n=10)
 #are higher than for pairs of points that are far apart
 
 #Se utiliza las variables latitude y longitude
-ozone.dists <- as.matrix(dist(cbind(ozone$Lon, ozone$Lat)))
+cwm_dists <- as.matrix(dist(cbind(data_cwm$longitude, data_cwm$latitude)))
 
-ozone.dists.inv <- 1/ozone.dists
+cwm_dists_inv <- 1/cwm_dists
 
-diag(ozone.dists.inv) <- 0
+diag(cwm_dists_inv) <- 0
 
 #La matriz debe quedar con 0s en la diagonal
-ozone.dists.inv[1:5, 1:5]
+cwm_dists_inv[1:5, 1:5]
 
 
 # Moran´s I ---------------------------------------------------------------
-
-Moran.I(ozone$Av8top, ozone.dists.inv)
-
 #Null hypothesis <--- No spatial autocorrelation
 
+#CWM af
+Moran.I(data_cwm$cwm_af, cwm_dists_inv)
 
+#CWM AFE
+Moran.I(data_cwm$cwm_afe, cwm_dists_inv)
 
+#CWM cfms
+Moran.I(data_cwm$cwm_cfms, cwm_dists_inv)
 
+#CWM DM
+Moran.I(data_cwm$cwm_dm, cwm_dists_inv)
+
+#CWM N
+Moran.I(data_cwm$cwm_n, cwm_dists_inv)
+
+#CWM P
+Moran.I(data_cwm$cwm_p, cwm_dists_inv)
 
