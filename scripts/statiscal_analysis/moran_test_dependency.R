6cd6429986dbcb1d7f86bd700e08785549836764
ecamo19 <erickcamo19@gmail.com>
2019-02-03 19:33:41 -0600

    Ref #5

--- scripts/statiscal_analysis/moran_test_dependency.R
+++ scripts/statiscal_analysis/moran_test_dependency.R
@@ -1,6 +1,10 @@
+rm(list=ls())
 
 
-
+# Objetivo ----------------------------------------------------------------
+#El objetivo de este script es desarrollar el ejemplo encontrado en
+#https://stats.idre.ucla.edu/r/faq/how-can-i-calculate-morans-i-in-r/
+#el cual hace la prueba de Moran para detectar dependecias espaciales 
 
 
 
