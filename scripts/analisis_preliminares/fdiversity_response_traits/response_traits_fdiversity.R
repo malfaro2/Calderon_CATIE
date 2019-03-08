f2a96f975e0907a50f9eeea29575e8ae441f4dd8
ecamo19 <erickcamo19@gmail.com>
2019-03-07 21:59:57 -0600

    Response Traits fdiversity

---
+++ scripts/analisis_preliminares/fdiversity_response_traits/response_traits_fdiversity.R
@@ -0,0 +1,81 @@
+rm(list = ls())
+
+# Objetivo ----------------------------------------------------------------
+#Calcular los indices de diversidad funcional FDis, FDiv, FEve, con los 
+#datos de rasgos de respuesta para todas las 127 parcelas del norte de 
+#Costa Rica mediante el paquete FD. 
+
+#Para el calculo de los indices se siguio el libro Functional and 
+#Phylogenetic Ecology in R. pp80
+
+#Paquetes
+library(FD)
+library(tidyverse)
+
+
+# Cargar paquetes ---------------------------------------------------------
+#Response traits
+dresp <- read.csv("data/raw/response_traits/data_respose_traits_final.csv", 
+                  header = T,row.names = 1)
+
+dresp <- dresp %>% 
+  select(-c(familia,especie))
+
+str(dresp)
+head(dresp)
+
+#Data abundancia
+dabund_relativa <- read.csv("data/clean/dabund_relativa.csv", 
+                            row.names = 1, header = T)
+
+# Cleaning data sets ------------------------------------------------------
+#Las especies en abundancias y rasgos tienen que tener el mismo orden para
+# que la funcion dbFD corra
+
+#Ver que las especies sean las mismas en ambos data sets
+n1 <- data.frame(as.factor(colnames(dabund_relativa)))
+colnames(n1) <- "especie" 
+class(n1)
+
+n2 <- data.frame(as.factor(dresp$coespec))
+colnames(n2) <- "especie" 
+class(n2)
+
+eliminar <- (anti_join(n1,n2, by="especie"))
+
+dim(dresp)
+
+dabund_rela_clean <- dabund_relativa %>% 
+  select(-c(ABARAD ,APEIME ,BALIEL ,BROSGU ,BROSLA,
+             CASEAR ,CECRIN ,CECROB ,COJOCO ,COUMMA,
+             CYNORE ,DIALGU ,ENTESC ,GRIACA ,GUETSP,
+             INGAAC ,INGAAE ,INGAAL ,INGACH ,INGAJI,
+             INGALE ,INGAMO ,INGAPE ,INGASE ,LICNAF,
+             LICNKA ,LICNSA ,LICNSP ,MICRME ,OCHRPY,
+             PACHAQ ,PODOGU ,POURBI ,POURMI ,POUTBE,
+             POUTCU ,POUTDU ,PSYCPA ,TAPIGU ,TOVOWE))
+
+dim(dabund_rela_clean)
+dim(dresp)
+
+#View(dresp$coespec)
+#View(colnames(dabund_rela_clean))
+target <- colnames(dabund_rela_clean) 
+dresp <- dresp[match(target, row.names(dresp)),]
+#View(dresp$coespec)
+
+# Indices de diversidad ---------------------------------------------------
+
+#Calcular indices de diversidad
+
+gowdist <- gowdis(dresp)
+
+indices <- dbFD(dresp[colnames(dabund_rela_clean),],
+                dabund_rela_clean, w.abun = T,stand.x = F,corr="cailliez")
+
+
+
+
+
+
+
